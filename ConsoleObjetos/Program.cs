812e66e132df9ef0a5e3d0ed7842e45346de3361
JorgeGAndujar <iyejorge@gmail.com>
2024-11-11 15:58:30 +0100

    SNAPSHOT 1

---
+++ ConsoleObjetos/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo1();
+            Ejemplo2();
+        }
+
+        public static void Ejemplo1()
+        {
+            List<Alumno1> alumnos_l = new List<Alumno1>
+            {
+                new Alumno1 { Nombre = "Luis", Edad = 41 },
+                new Alumno1 { Nombre = "Marta", Edad = 40 },
+            };
+
+            foreach (Alumno1 alumno in alumnos_l)
+            {
+                Console.WriteLine(alumno.Nombre);
+                Console.WriteLine(alumno.Edad);
+            }
+        }
+        public static void Ejemplo2()
+        {
+            List<Alumno2> alumnos_l = new List<Alumno2>
+            {
+                new Alumno2("Pablo", 33),
+                new Alumno2("Rafa",33),
+            };
+
+            foreach (Alumno2 alumno in alumnos_l)
+            {
+                Console.WriteLine(alumno.Nombre);
+                Console.WriteLine(alumno.Edad);
+            }
+        }
+    }
+
+    public class Alumno1
+    {
+        public string Nombre {  get; set; }
+        public int Edad { get; set; }
+    }
+    public class Alumno2
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Alumno2(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+    }
+}
---
+++ ProyectoVentanaMysql/MenuWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace ProyectoVentanaMysql
+{
+    /// <summary>
+    /// Lógica de interacción para MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            ProyectoVentanaMysql.BuscarProductoPorIdComboBox.MainWindow vbuscar = new ProyectoVentanaMysql.BuscarProductoPorIdComboBox.MainWindow();
+            vbuscar.Show();
+        }
+
+        private void BtnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            ProyectoVentanaMysql.MostrarTodosProductosDataGrid.MainWindow vmostrar = new ProyectoVentanaMysql.MostrarTodosProductosDataGrid.MainWindow();
+            vmostrar.Show();
+        }
+
+        private void BtnBuscarId_Click(object sender, RoutedEventArgs e)
+        {
+            ProyectoVentanaMysql.MainWindow2 vbuscarid = new ProyectoVentanaMysql.MainWindow2();
+            vbuscarid.Show();
+        }
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnInsertarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            ProyectoVentanaMysql.GestionUsuarios.WindowGestionUsuarios vinsertar = new ProyectoVentanaMysql.GestionUsuarios.WindowGestionUsuarios();
+            vinsertar.Show();
+        }
+
+        private void BtnMostrarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            ProyectoVentanaMysql.MostarUsuarios.MostrarUsuariosWindow vmostrarusuarios = new ProyectoVentanaMysql.MostarUsuarios.MostrarUsuariosWindow();
+            vmostrarusuarios.Show();
+        }
+    }
+}
